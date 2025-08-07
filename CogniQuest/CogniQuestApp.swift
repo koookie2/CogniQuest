494f3e6df896e9acb485f3904a0eb9ad4c2bc258
kookie2 <kavinjgodwin@gmail.com>
2025-08-07 13:43:33 -0400

    Initial Commit

---
+++ CogniQuest/CogniQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CogniQuestApp.swift
+//  CogniQuest
+//
+//  Created by Kavin on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CogniQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CogniQuestTests/CogniQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CogniQuestTests.swift
+//  CogniQuestTests
+//
+//  Created by Kavin on 8/7/25.
+//
+
+import Testing
+@testable import CogniQuest
+
+struct CogniQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
